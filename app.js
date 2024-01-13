20e32071c5f3d4c67e4a7064dd2562c6cf9fbb6f
Pedro Castaneda <petecast76@gmail.com>
2024-01-13 01:05:27 -0800

    feat: saved dom elements to variables

---
+++ app.js
@@ -0,0 +1,5 @@
+const body = document.body;
+const slides = document.querySelectorAll('.slide');
+
+const leftBtn = document.getElementById('left');
+const rightBtn = document.getElementById('right');
